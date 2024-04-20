df9f150b8bdf41f270dcc3b1e8b8f341003413aa
soatra <johnsoatra@gmail.com>
2024-04-20 09:07:17 +0700

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,61 @@ import { useEffect, useState } from "react";
 
 function App() {
     const [start, setStart] = useState(false);
-    const [duration, setDuration] = useState(2000);
+    // const [duration, setDuration] = useState(2000);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setStart(true);
-        }, 2000);
-    }, []);
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setStart(true);
+    //     }, 2000);
+    // }, []);
 
     return (
-        <div>
-            <div
-                style={{
-                    transitionProperty: 'all',
-                    transitionDuration: duration + "ms",
-                    transitionTimingFunction: 'linear',
-                }}
-                className={`
-                    aspect-square w-[50px] bg-blue-300
-                    ${
-                        start ? 'translate-x-0' : 'translate-x-[300px]'
+        <div
+            className="flex justify-center p-10">
+            <Any
+                className="w-fit"
+                from="translate-x-[20px]"
+                to={[
+                    {
+                        state: 'translate-x-[-20px]',
+                        easing: 'linear',
+                        duration: 3000,
+                        on: {
+                            complete: 0.91,
+                            task: () => {
+                                setStart(true);
+                            }
+                        }
                     }
-                `}>
-            </div>
-            <br /><br />
-            <button onClick={() => setDuration(500)}>Update</button>
+                ]}>
+                <Any
+                    from="translate-x-[200px]"
+                    to={[
+                        {
+                            state: 'translate-x-[-200px]',
+                            easing: Easings.Cubic(0, 0.82, 1, 0.19),
+                            duration: 3000
+                        }
+                    ]}>
+                    <Any
+                        from="opacity-0"
+                        to={[
+                            {
+                                state: 'opacity-100',
+                                easing: Easings.ExpoOut,
+                                duration: 250
+                            },
+                            {
+                                start: start,
+                                state: 'opacity-0',
+                                easing: 'linear',
+                                duration: 250
+                            }
+                        ]}>
+                        <p>Helloworld</p>
+                    </Any>
+                </Any>
+            </Any>
         </div>
     );
 }
