67de85f91e6dc599fb1f308f7260848b25436331
soatra <johnsoatra@gmail.com>
2024-05-15 10:08:24 +0700

    add export type Easing

--- src/exports/static.ts
+++ src/exports/static.ts
@@ -1,7 +1,9 @@
 import { type AnyProps } from '../components/Any';
+import { type Easing } from '../types';
 import { Easings } from '../constants/vars';
 
 export {
     Easings,
     type AnyProps,
+    type Easing,
 };
